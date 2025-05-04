aedf3270bf11e8f3998891c3897e529190bb8df6
Sudharsan GS <g.s.sudharsan99@gmail.com>
2025-05-04 15:21:25 +0530

    dockerize the application

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // Optimized for Docker
+  swcMinify: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
